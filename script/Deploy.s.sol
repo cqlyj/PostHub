9f4e836fcde81fec9701574c5d97b9a7c56dc57c
cqlyj <luoyingjie0721@gmail.com>
2025-07-05 02:51:48 +0200

    add the deploy script for customization contract

---
+++ script/Deploy.s.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.28;
+
+import {Script, console} from "forge-std/Script.sol";
+import {Customization} from "../src/Customization.sol";
+
+contract DeployCelo is Script {
+    // @TODO: Add restricted nationalities
+    string[] restrictedNationalities = [""];
+
+    function run() external {
+        vm.startBroadcast();
+
+        // Deploy the Customization contract
+        address customization = address(
+            new Customization(
+                0x68c931C9a534D37aa78094877F46fE46a49F1A51,
+                0,
+                restrictedNationalities
+            )
+        );
+        console.log("Customization deployed at:", customization);
+
+        vm.stopBroadcast();
+    }
+}
